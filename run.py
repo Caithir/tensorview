1b2989b56bdd21d2fc9d9bae55a4e531f31a7422
rachshap <rachel.shapiro02@gmail.com>
2018-10-30 16:21:48 -0400

    Basic layout of website with forms for adding in modifiable parameters

--- run.py
+++ run.py
@@ -5,9 +5,8 @@ app = Flask(__name__)
 @app.route("/")
 def template_test():
     kwargs = {"my_string":"Table 1",
-              'username': 123,
-              'my_list':[[1, 1, 'Hi', 'Princess', 9], [5], ['rachel', 89, 2]],
-              'fillups': [['   '],['wild', 'this'], ['turtle', 'colter']]}
+              'my_list':['   ','Col1', 'Col2', 'Col3', 'Col4'],
+              'fillups': [['Run1', 'test ', 'next'],['Run2','row2', 'row2.5'], ['row3'], ['row4']]}
     return render_template('index.html', **kwargs)
 
 if __name__ == '__main__':
