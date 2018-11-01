ecaafc0050e45e07d4bf93f7d8c4900842de7ad9
rachshap <rachel.shapiro02@gmail.com>
2018-11-01 18:10:09 -0400

    Added in the drop down options and conditionally choosing which options based on types.

--- run.py
+++ run.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def template_test():
     kwargs = {"my_string":"Table 1",
               'my_list':['   ','Col1', 'Col2', 'Col3', 'Col4'],
-              'fillups': [['Run1', 'test ', 'next'],['Run2','row2', 'row2.5'], ['row3'], ['row4']]}
+              'fillups': [['Run1', 0, 12, "test", "turtle"],['Run2','row2', 'row2.5', '23', 'open'], ['row3', '12', 0, 0, 9], ['row4', 21, 't', 12, '9']]}
     return render_template('index.html', **kwargs)
 
 if __name__ == '__main__':
