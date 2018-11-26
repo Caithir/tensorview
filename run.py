b223f47a146160d92fb3465a287aac0db590c975
rachshap <rachel.shapiro02@gmail.com>
2018-11-26 16:52:34 -0500

    got the regex string to update.

--- run.py
+++ run.py
@@ -2,12 +2,15 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", methods=['Get', 'Post'])
 def template_test():
-    kwargs = {"my_string":"Table 1",
-              'my_list':['   ','Col1', 'Col2', 'Col3', 'Col4'],
-              'fillups': [['Run1', 0, 12, "test", "turtle"],['Run2','row2', 'row2.5', '23', 'open'], ['row3', '12', 0, 0, 9], ['row4', 21, 't', 12, '9']]}
+    kwargs = {"table_name": "Table 1",
+              'headings': ['   ','Col1', 'Col2', 'Col3', 'Col4'],
+              'table_contents': [['Run1', 0, 12, "test", "turtle"],['Run2','row2', 'row2.5', '23', 'open'], ['row3', '12', 0, 0, 9], ['row4', 21, 't', 12, '9']]}
     return render_template('index.html', **kwargs)
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
