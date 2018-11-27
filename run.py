9e3c1dbc2928e8282a0e1b95f4ddef35c04a7fc6
rachshap <rachel.shapiro02@gmail.com>
2018-11-27 18:27:49 -0500

    jinja is not my friend.

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
@@ -6,11 +6,20 @@ app = Flask(__name__)
 def template_test():
     kwargs = {"table_name": "Table 1",
               'headings': ['   ','Col1', 'Col2', 'Col3', 'Col4'],
-              'table_contents': [['Run1', 0, 12, "test", "turtle"],['Run2','row2', 'row2.5', '23', 'open'], ['row3', '12', 0, 0, 9], ['row4', 21, 't', 12, '9']]}
+              'table_contents': [['Run1', 0, 12, "test", "turtle"],['Run2','row2', 'row2.5', '23', 'open'], ['row3', '12', 0, 0, 9], ['row4', 21, 't', 12, '9']],
+              'results': request.form}
     return render_template('index.html', **kwargs)
 
 
+# @app.route('/result',methods = ['POST', 'GET'])
+# def result():
+#    if request.method == 'POST':
+#       result = request.form
+#       return render_template("result.html",result = result)
 
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
