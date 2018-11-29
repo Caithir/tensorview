fe2ef603c5e453f7b82f5561ef681f0434153570
Colter <colter.norick@gmail.com>
2018-11-29 13:02:56 -0500

    merge commit and push

--- util.py
+++ util.py
@@ -4,8 +4,6 @@ from urllib.parse import unquote
 class QueryConverter(BaseConverter):
 
     def to_python(self, value):
-        print(value)
-        value = unquote(value)
         return [Query(*query.split("_~_")) for query in value.split("|")]
 
     def to_url(self, values):
